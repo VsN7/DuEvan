c86a535c474f38e99b03c99431c8630f4647fc46
Vitor Santos <vitor7_sn@hotmail.com>
2019-08-07 10:58:44 -0300

    Ajustando efeitos navBar

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,18 +1,58 @@
-import "../Bootstrap/jquery/dist/jquery.js";
 
 // Evento de Navegadores
+$(document).ready(function(){
+    let arquivo = $("#arquivo");
+    let cliente = $("#cliente");
+    let servico = $("#servico");
+    let orcamento = $("#orcamento");
+    let os = $("#os");
+    let forcenedores = $("#fornecedores");
+    let relatorios = $("#relatorios");
+    let verificaNavClick = $(".nav-item dropdown");
+    // arquivo.on("click", function(e){
+    //     e.preventDefault();
+    //     arquivo.css({"background-color":"rgba(197, 167, 161)", "color":"black"});
+    //     cliente.css({"color":"rgba(255, 255, 255, 0.5)","background-color":"rgba(14, 2, 2, 0.829)"});
+    // },function(e){
+    //     e.preventDefault();
+    //     cliente.css({"background-color":"rgba(197, 167, 161)", "color":"black"});
+    //     arquivo.css({"color":"rgba(255, 255, 255, 0.5)","background-color":"rgba(14, 2, 2, 0.829)"});
+    // }
+    // );
+    // cliente.click( 
+    //     function(e){
+    //         cliente.css({"background-color":"rgba(197, 167, 161)", "color":"black"});
+            
+    //     }
+    // );
 
-let arquivo = document.getElementById("arquivo");
+    // $(".nav-link").hover(function(e){
+    //     e.preventDefault();
+    //     $(".dropdown").addClass("focusNavBar");
+    // });
 
-function clickArquivo(){
-    arquivo.style = "background-color: rgba(197, 167, 161);"+ 
-                    "color: black";
-    
-}
-function offClick(){
-    arquivo.style = "background-color:rgba(197, 167, 161); "+
-                    "color: white";
-}
+
+
+//     $(".nav-link").on("click",function(e){
+//         e.preventDefault();
+//         $(".dropdown").addClass("focusNavBar");
+        
+//         $(".dropdown").hover(
+//             function(e){
+
+//             },
+//             function(e){
+//                 $(".version").click();
+//             }
+//         );
+        
+        
+//     });
+
+    $(".close").on("click", function(e){
+        $(".tamanhoOpt").removeClass("show");
+    })
+});
 
 
 
